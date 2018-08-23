a45bff5bbefea096633dfd1e6f79712a94a72709
Luis Jarman <lujarman@gmail.com>
2018-08-23 21:01:15 +0200

    adds empty home component

--- shopoodle/src/app/app.module.ts
+++ shopoodle/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {ShoppingCartModule} from 'ng-shopping-cart'; // <-- Import the module class
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ShoppingCartModule.forRoot({ // <-- Add the cart module to your root module
+        // itemType: MyCartItemClass, // <-- Configuration is optional
+        // serviceType: 'localStorage',
+        // serviceOptions: {
+        //     storageKey: 'NgShoppingCart',
+        //     clearOnError: true
+        // }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
